761550ec6adee60329a23b57c2d61f0d5b9ba39e
Satoshi Terada <st55334@gmail.com>
2023-10-26 18:22:12 +0900

    [update] webpack の更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const path = require('path')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const ImageMinimizer = require('image-minimizer-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const mode = process.env.NODE_ENV
+const enabledSourceMap =  mode !== 'production'
 
 const config = {
   mode: mode,
@@ -23,14 +26,43 @@ const config = {
       files: ['./*.php', './**/*.php'],
       injectChanges: true,
     }),
+    new ESLintPlugin({
+      extensions: ['.js'],
+      exclude: 'node_modules'
+    }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/assets/images'),
-          to: 'images/[path][name][ext]',
+          to: 'images/[name][ext]',
           noErrorOnMissing: true,
+          globOptions: {
+            ignore: ['**/assets/images/sprite/**.svg']
+          }
         },
-      ],
+        {
+          from: path.resolve(__dirname, 'src/assets/images/sprite'),
+          to: 'images/sprite/[name][ext]',
+          noErrorOnMissing: true,
+        }
+      ]
+    }),
+    new ImageMinimizer ({
+      test: /\.(png|jpe?g)$/i,
+      generator: [{
+        type: 'asset',
+        implementation: ImageMinimizer.imageminGenerate,
+        options: {
+          plugins: [
+            ['webp', { quality: 85 }],
+          ],
+          encodeOptions: {
+            webp: {
+              lossless: 1,
+            },
+          },
+        },
+      }]
     }),
     new MiniCssExtractPlugin({
       filename: './stylesheets/[name].css',
@@ -44,13 +76,66 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '../' },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+              importLoaders: 2,
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['autoprefixer', { grid: true }],
+                  ['postcss-preset-env', { stage: 3 }],
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap
+            },
+          },
+        ]
       },
       {
         test: /\.(jpg|png|svg|gif)$/,
-        type: 'asset/inline',
+        type: 'asset',
       },
+      {
+        test: /\.svg$/,
+        include: [
+          path.resolve(__dirname, "src/assets/images/sprite")
+        ],
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                {
+                  name: "removeAttrs",
+                  params: {
+                    attrs: "(fill|stroke)"
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
     ],
   },
 }
