4af5af6a862888b3801a16c6042964ba03448ee9
Satoshi Terada <st55334@gmail.com>
2023-10-26 18:26:29 +0900

    [add] WordPressのテンプレート初期ファイルを追加

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+  this is footer
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main data-barba="container" data-barba-namespace="home">
+  <p>this is home</p>
+  <hr>
+  <p><a href="/page/">→next page link.</a></p>
+  <ul>
+    <li>あいうお</li>
+  </ul>
+</main>
+
+<?php get_footer(); ?>
